4dfe96e12ae9f7e4654af6cfd776500ae72f1755
Zachary Marcus <ZachMarcus@users.noreply.github.com>
2018-03-31 13:20:57 -0700

    Fixed cross-platform smoothscroll
    Removed external smoothscroll in favor of classic easing functions and some good ol jquery; removed old canvas js.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 /* eslint-env browser */
 (function () {
 	'use strict';
-
-
 	// Burger menu toggle
 	var burger = document.querySelector('.nav-toggle');
 	var menu = document.querySelector('.nav-menu');
@@ -10,8 +8,6 @@
 		burger.classList.toggle('is-active');
 		menu.classList.toggle('is-active');
 	});
-
-
 })();
 
 (function () {
@@ -23,42 +19,10 @@
 			document.querySelector('#scroll-hint').classList.add('hidden');
 		}
 	}
-
 	document.addEventListener('scroll', hideScrollHint, {passive: true});
 })();
-
-
-
-
-(function () {
-	'use strict';
-	function makeCanvas() {
-		try {
-			TagCanvas.Start('myCanvas', 'tags',{
-				textColour: null,
-				outlineColour: '#363636',
-				reverse: true,
-				pinchZoom: true,
-				depth: 1.1,
-				textHeight:26,
-				zoom: 0.8,
-				freezeDecel: true,
-				minspeed: 0.01,
-				maxSpeed: 0.08,
-				initial: [0.2,-0.1],
-				decel: 0.01
-			});
-		} catch(e) {
-			//something went wrong, hide the canvas container
-			document.getElementById('myCanvasContainer').style.display = 'none';
-		}
-	}
-	document.addEventListener('DOMContentLoaded', makeCanvas);
-})();
-
 
 function scrollToDiv(element) {
 	element = element.replace("link", "");
 	$('html,body').unbind().animate({scrollTop: $(element).offset().top-50},'slow');
 }
-
